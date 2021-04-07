2575b2e655917ee9dcb3a10303c7dc53ee4d699a
jennafritz <jennafritz24@gmail.com>
2021-04-07 17:25:03 -0400

    commit tree ee678e4f03ecc111e73b870a0d9efdedc95cd89f

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(driverNames, searchName) {
+    return driverNames.filter(function(currentName) {
+        return currentName.toLowerCase() === searchName.toLowerCase()
+    })
+}
+
+function fuzzyMatch(driverNames, searchNamePartial) {
+    return driverNames.filter(function(currentName) {
+        return currentName[0].toLowerCase() === searchNamePartial[0].toLowerCase()
+    })
+}
+
+function matchName(driverInfo, searchNameObj) {
+    return driverInfo.filter(function(currentDriver) {
+        return currentDriver.name === searchNameObj
+    })
+}
